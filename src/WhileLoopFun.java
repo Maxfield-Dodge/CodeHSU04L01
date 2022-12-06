31b2da8dbb0c17f2622ebf4b9c68c3703bd64333
Maxfield-Dodge <dodgem@wlhs.wlwv.k12.or.us>
2022-12-06 09:53:15 -0800

    added a bunch of different while loops

--- src/WhileLoopFun.java
+++ src/WhileLoopFun.java
@@ -1,5 +1,5 @@
 
-/*
+ /*
     Template for while loop
 
     //initialize loop control variable
@@ -11,26 +11,57 @@
  */
 
 
-public class WhileLoopFun {
+ import java.sql.SQLOutput;
+
+ public class WhileLoopFun {
 
     public static void main(String[] args) {
         //count to 10
 
+        int count = 1;
+        while(count<=10){
+            System.out.print(count + " ");
+            count++;
+        }
+        System.out.println();
 
         //what does this code loop do? Do this without running it
-        int count = 2;
+        count = 2;
         while(count <= 100){
             System.out.print(count + " ");
             count *= 2;
         }
         System.out.println();
+
         //write a loop that counts by 5 until you get to 100
+        int startCount = 0;
+        while(startCount <= 100){
+            System.out.print(startCount + " ");
+            startCount += 5;
+        }
+        System.out.println();
 
         //write a loop that counts down from 10 to 1 then prints "Blast off!"
+        int countdown = 10;
+        while(countdown>=1){
+            System.out.print(countdown + " ");
+            if(countdown == 1) {
+                System.out.println("Blast off!");
+            }
+            countdown--;
+        }
 
         //loop mistakes and while vs do while loop
         //make an infinite loop happen
+        int newCount = 5;
+        while(newCount >= 10){
+            System.out.println("This statement has executed.");
+        }
 
+        //outside of CS A scope
+        do {
+            System.out.println("This wil print at least once.");
+        }while(newCount >= 10);
 
     }
 }
